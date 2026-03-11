ca81d74ff18aba5ce50799a0a03ffab5756384d3
annybestrizcarvalhoduarte <anny.carvalho.duarte@escola.pr.gov.br>
2026-03-11 15:20:16 -0300

    sketch123

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
+let terra;
+
+function preload(){
+    terra = loadImage('terra.jpg')
+}
 function setup() {
     createCanvas(1920,1080, WEBGL);//plano 3D
-    terra=loadImage('terrajpg')
 }
 
 function draw() {
     background(205, 102, 94);//cor de fundo
     rotateY(millis()/1000);//rotação a terra
-    TextTure(terra);//textura da terra, imagem.
+    textTure(terra);//textura da terra, imagem.
     Sphere (80, 100);//esera 3D
 }
